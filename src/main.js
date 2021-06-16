4aef2b8188472a3a79a44a9b4f39540cf231685f
li-2019220129 <3312202467@qq.com>
2021-06-16 10:06:37 +0800

    环境设置

--- src/main.js
+++ src/main.js
@@ -1,9 +1,27 @@
 import Vue from 'vue'
 import App from './App.vue'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
 import router from './router'
-
+Vue.use(VueAxios,axios);
 Vue.config.productionTip = false
-
+axios.defaults.baseURL = '/api';
+axios.defaults.timeout = 8000; 
+axios.interceptors.response.use(function(response){
+  let res = response.data;
+  let path = location.hash;
+  if(res.status == 0){
+    return res.data;
+  }else if(res.status == 10){
+    if (path != '#/index'){
+      window.location.href = '/#/login';
+    }
+    return Promise.reject(res);
+  }else{
+    alert('错误')
+    return Promise.reject(res);
+  }
+})
 new Vue({
   router,
   render: h => h(App)
