bd45364e6e47fde8611b17bea88ec7d7b82b80f6
li-2019220129 <3312202467@qq.com>
2021-07-12 19:37:02 +0800

    最终完成

--- vue.config.js
+++ vue.config.js
@@ -16,7 +16,9 @@ module.exports = {
   // outputDir:'dist',
   // indexPath:'index2.html',
   // lintOnSave:false,
+  // 设置为false将无法看到源码
   productionSourceMap:true,
+  //防止预加载
   chainWebpack:(config)=>{
     config.plugins.delete('prefetch');
   }
